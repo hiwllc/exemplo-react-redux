26f8bd5d4379b994d50bc13c2afcbc90714a3c9a
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-13 23:02:40 -0300

    adiciona configuracoes iniciais com o json-server

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <ul>
+      <li>
+        <div>
+          <h3>Member</h3>
+          <button>Delete Member</button>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default App;
