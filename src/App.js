4a3f567a60602ecb28e8a4e2b88fc24a9c2ad20f
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-14 18:16:40 -0300

    fazemos o mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-function App() {
+import { connect } from "react-redux";
+
+/**
+ * esse languages vem ali do mapStateToProps
+ */
+function App({ languages }) {
   return (
-    <ul>
-      <li>
-        <div>
-          <h3>Member</h3>
-          <button>Delete Member</button>
-        </div>
-      </li>
-    </ul>
+    <>
+      <div>
+        <input name="cadastra uma lang nova aqui" />
+        <button>adicionar</button>
+      </div>
+
+      <hr />
+      <h3>Linguagems</h3>
+      {languages.length <= 0 && <span>Opa não tem nada ainda :(</span>}
+
+      <ul>
+        {languages.map((language) => (
+          <li key={language.id}>
+            <div>
+              <h3>{language.name}</h3>
+              <button>Delete Member</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App;
+const mapStateToProps = ({ languages }) => ({
+  /**
+   * Aqui nós mapeamos o languages que vem lá do nosso reducer, inicialmente será um array vazio.
+   */
+  languages,
+});
+
+export default connect(mapStateToProps)(App);
